f44b19d36cd3a0ac598efd71af39a31d7fe91e3b
Annisa Nabila Nasution <annisanabila14@gmail.com>
2020-10-04 12:26:54 +0700

    Initial Commit

---
+++ MovieDatabase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieDatabase
+//
+//  Created by Annisa Nabila Nasution on 04/10/20.
+//  Copyright © 2020 Annisa Nabila Nasution. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
